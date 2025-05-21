cd3fbc7e66bb9a37547900ff0ff12357523fb547
FiantsoHarena <fiantsorav@gmail.com>
2025-05-21 08:48:16 +0300

    fix: enhanced error handling

--- Main.php
+++ Main.php
@@ -1,69 +1,102 @@
 <?php
 declare(strict_types=1);
 
-class UserNotFoundException extends Exception {}
-
-class Logger {
-    public function log(string $message) {
-        file_put_contents('error.log', $message.PHP_EOL, FILE_APPEND);
-    }
+class UserNotFoundException extends Exception
+{
 }
 
-const USERS = [
-    "1" => "Julien",
-    "2" => "Rajerison",
-    "3" => "Jul",
-];
+class Logger
+{
+    private $logFile;
 
-class UserRepository {
-    public function findUser(string $id) {
-        return USERS[$id] ?? null;
-    }
-
-    public function getUserById(string $id) {
-        $log = new Logger();
-        try {
-            $user = $this->findUser($id);
-            if (!$user) {
-                throw new UserNotFoundException("User with $id not found !");
-            }
-
-            return $user;
-        } catch (UserNotFoundException $exception) {
-            $log->log($exception->getMessage());
-
-            return null;
-        } catch (Exception $exception) {
-            $log->log($exception->getMessage());
-
-            return null;
-        }
-    }
-}
-
-class Controller {
-    public function getCurrentUser(string $id) : ?string 
+    public function __construct($logFile = 'error.log')
     {
-        $log = new Logger();
+        $this->logFile = $logFile;
+    }
+
+    public function log(string $message)
+    {
+        file_put_contents($this->logFile, $message . PHP_EOL, FILE_APPEND);
+    }
+}
+
+class UserDataStore
+{
+    private $users;
+
+    public function __construct(array $users = [])
+    {
+        $this->users = !empty($users) ? $users : [
+            "1" => "Julien",
+            "2" => "Rajerison",
+            "3" => "Jul",
+        ];
+    }
+
+    public function getUsers(): array
+    {
+        return $this->users;
+    }
+}
+
+class UserRepository
+{
+    private $dataStore;
+    private $logger;
+
+    public function __construct(UserDataStore $dataStore, Logger $logger)
+    {
+        $this->dataStore = $dataStore;
+        $this->logger = $logger;
+    }
+
+    public function findUser(string $id)
+    {
+        $users = $this->dataStore->getUsers();
+        return $users[$id] ?? null;
+    }
+
+    public function getUserById(string $id): string
+    {
+        $user = $this->findUser($id);
+        if (!$user) {
+            $errorMessage = "User with ID $id not found!";
+            $this->logger->log("Repository: $errorMessage");
+            throw new UserNotFoundException($errorMessage);
+        }
+
+        return $user;
+    }
+}
+
+class Controller
+{
+    private $repository;
+    private $logger;
+
+    public function __construct(UserRepository $repository, Logger $logger)
+    {
+        $this->repository = $repository;
+        $this->logger = $logger;
+    }
+    
+    public function getCurrentUser(string $id): string
+    {
         try {
-            $repository = new UserRepository();
-            $user = $repository->getUserById($id);
-            if (!$user) {
-                throw new UserNotFoundException();
-            }
-            
-            return $user;
+            return $this->repository->getUserById($id);
         } catch (UserNotFoundException $e) {
-            $log->log("Controller, $id user is not found !");
-
-            return "User not found !";
-        } catch (Exception $exception) {
-            $log->log("Internal serveur error, {$exception->getMessage()}");
-
-            return "Une erreur est survenue !";
+            $this->logger->log("Controller: {$e->getMessage()}");
+            return "User not found!";
+        } catch (Exception $e) {
+            $this->logger->log("Internal server error: {$e->getMessage()}");
+            return "An error occurred!";
         }
     }
 }
 
-$main = new Controller();
-print_r($main->getCurrentUser(7));
+$logger = new Logger();
+$dataStore = new UserDataStore();
+$repository = new UserRepository($dataStore, $logger);
+$controller = new Controller($repository, $logger);
+
+print_r($controller->getCurrentUser("7"));
