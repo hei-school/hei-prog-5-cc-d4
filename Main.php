fe1f91a9c6375af29675c8838d3d473f5900e64c
Ramilison Michel Heather <hei.michel.8@gmail.com>
2025-05-21 08:59:39 +0300

    Remove unnecessary exception in getUserById function

--- Main.php
+++ Main.php
@@ -1,10 +1,14 @@
 <?php
 
-class UserNotFoundException extends Exception {}
+class UserNotFoundException extends Exception
+{
+}
 
-class Logger {
-    public function log(string $message) {
-        file_put_contents('error.log', $message.PHP_EOL, FILE_APPEND);
+class Logger
+{
+    public function log(string $message)
+    {
+        file_put_contents('error.log', $message . PHP_EOL, FILE_APPEND);
     }
 }
 
@@ -14,12 +18,15 @@ const USERS = [
     "3" => "Jul",
 ];
 
-class UserRepository {
-    public function findUser(string $id) {
+class UserRepository
+{
+    public function findUser(string $id)
+    {
         return USERS[$id] ?? null;
     }
 
-    public function getUserById(string $id) {
+    public function getUserById(string $id)
+    {
         $log = new Logger();
         try {
             $user = $this->findUser($id);
@@ -31,13 +38,15 @@ class UserRepository {
             return $user;
         } catch (UserNotFoundException $exception) {
             $log->log($exception->getMessage());
-            
+
             return null;
+        }
     }
 }
 
-class Controller {
-    public function getCurrentUser(string $id) : ?string 
+class Controller
+{
+    public function getCurrentUser(string $id): ?string
     {
         $log = new Logger();
         try {
@@ -46,7 +55,7 @@ class Controller {
             if (!$user) {
                 throw new UserNotFoundException();
             }
-            
+
             return $user;
         } catch (UserNotFoundException $e) {
             $log->log("Controller, $id user is not found !");
@@ -54,7 +63,7 @@ class Controller {
             return "User not found !";
         } catch (Exception $exception) {
             // Use English in logs for better consistency and readability
-            $log->log("Internal server error, {$exception->getMessage()}"); 
+            $log->log("Internal server error, {$exception->getMessage()}");
 
             return "An error occurred!";
         }
