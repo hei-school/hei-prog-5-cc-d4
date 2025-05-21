65b2cc6632e4109e337e7073b64bf3b969116c6c
mvjdev <hei.maria.625@gmail.com>
2025-05-21 08:26:55 +0300

    fix: error Exception

--- Main.php
+++ Main.php
@@ -2,9 +2,11 @@
 
 class UserNotFoundException extends Exception {}
 
-class Logger {
-    public function log(string $message) {
-        file_put_contents('error.log', $message.PHP_EOL, FILE_APPEND);
+class Logger
+{
+    public function log(string $message)
+    {
+        file_put_contents('error.log', $message . PHP_EOL, FILE_APPEND);
     }
 }
 
@@ -14,12 +16,15 @@ const USERS = [
     "3" => "Jul",
 ];
 
-class UserRepository {
-    public function findUser(string $id) {
+class UserRepository
+{
+    public function findUser(string $id)
+    {
         return USERS[$id] ?? null;
     }
 
-    public function getUserById(string $id) {
+    public function getUserById(string $id)
+    {
         $log = new Logger();
         try {
             $user = $this->findUser($id);
@@ -30,39 +35,31 @@ class UserRepository {
             return $user;
         } catch (UserNotFoundException $exception) {
             $log->log($exception->getMessage());
-            
-            return null;
-        } catch (Exception $exception) {
-            $log->log($exception->getMessage());
 
             return null;
         }
     }
 }
 
-class Controller {
-    public function getCurrentUser(string $id) : ?string 
+class Controller
+{
+    public function getCurrentUser(string $id): ?string
     {
         $log = new Logger();
         try {
             $repository = new UserRepository();
             $user = $repository->getUserById($id);
             if (!$user) {
-                throw new UserNotFoundException();
+                throw new UserNotFoundException("User with $id not found !");
             }
-            
+
             return $user;
         } catch (UserNotFoundException $e) {
             $log->log("Controller, $id user is not found !");
-
-            return "User not found !";
-        } catch (Exception $exception) {
-            $log->log("Internal serveur error, {$exception->getMessage()}");
-
-            return "Une erreur est survenue !";
+            return "User not with found !";
         }
     }
 }
 
 $main = new Controller();
-print_r($main->getCurrentUser(7));
+print_r($main->getCurrentUser(6));
