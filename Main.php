45e51612304eb28b09203e4776bb3e97ab9e0cff
jrajerison <jrajerison@bocasay.com>
2025-05-21 08:23:36 +0300

    force type checking

--- Main.php
+++ Main.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types=1);
 
 class UserNotFoundException extends Exception {}
 
@@ -30,7 +31,7 @@ class UserRepository {
             return $user;
         } catch (UserNotFoundException $exception) {
             $log->log($exception->getMessage());
-            
+
             return null;
         } catch (Exception $exception) {
             $log->log($exception->getMessage());
