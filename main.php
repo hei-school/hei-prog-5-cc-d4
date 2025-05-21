f251ed96b559c8b7e3c7a545af9d5fa915312423
mvjdev <hei.maria.625@gmail.com>
2025-05-21 08:26:55 +0300

    fix: error Exception

--- main.php
+++ main.php
@@ -10,32 +10,32 @@ class Logger
     }
 }
 
-const USERS = [
-    "1" => "Julien",
-    "2" => "Rajerison",
-    "3" => "Jul",
-];
-
 class UserRepository
 {
+    private const USERS = [
+        "1" => "Julien",
+        "2" => "Rajerison",
+        "3" => "Jul",
+    ];
+
     public function findUser(string $id): ?string
     {
-        return USERS[$id] ?? null;
+        return self::USERS[$id] ?? null;
     }
 
     public function getUserById(string $id): ?string
     {
-        $log = new Logger();
+        $logger = new Logger();
+
         try {
             $user = $this->findUser($id);
             if (!$user) {
-                throw new UserNotFoundException("User with $id not found !");
+                throw new UserNotFoundException("User with ID $id not found (from Repository).");
             }
 
             return $user;
-        } catch (UserNotFoundException $exception) {
-            $log->log($exception->getMessage());
-
+        } catch (UserNotFoundException $e) {
+            $logger->log($e->getMessage());
             return null;
         }
     }
@@ -45,22 +45,22 @@ class Controller
 {
     public function getCurrentUser(string $id): ?string
     {
-        $log = new Logger();
+        $logger = new Logger();
         $repository = new UserRepository();
-        
+
         try {
             $user = $repository->getUserById($id);
             if (!$user) {
-                throw new UserNotFoundException("User with $id not found !");
+                throw new UserNotFoundException("User with ID $id not found (from Controller).");
             }
 
             return $user;
         } catch (UserNotFoundException $e) {
-            $log->log("Controller, $id user is not found !");
-            return "User not with $id id not found !";
+            $logger->log($e->getMessage());
+            return "User with ID $id not found!";
         }
     }
 }
 
 $main = new Controller();
-print_r($main->getCurrentUser(6));
+echo $main->getCurrentUser("6");
