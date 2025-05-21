a6c8501514d89222e6aa7145a4e8c31cd6b50d34
AndoKami <hei.ando.1007@gmail.com>
2025-05-21 08:44:11 +0300

    fix : review the main code

--- Main.php
+++ Main.php
@@ -5,6 +5,7 @@ class UserNotFoundException extends Exception {}
 class Logger {
     public function log(string $message) {
         file_put_contents('error.log', $message.PHP_EOL, FILE_APPEND);
+        // ici le chemin du log est codé en dur
     }
 }
 
@@ -12,28 +13,34 @@ const USERS = [
     "1" => "Julien",
     "2" => "Rajerison",
     "3" => "Jul",
+    // utilisation d'une constante globale pour stocker les utilisateurs.
 ];
 
 class UserRepository {
     public function findUser(string $id) {
         return USERS[$id] ?? null;
+        // accès direct à la constante globale USERS.
     }
 
     public function getUserById(string $id) {
         $log = new Logger();
+        // instanciation répété de la classe Logger
         try {
             $user = $this->findUser($id);
             if (!$user) {
                 throw new UserNotFoundException("User with $id not found !");
+                // lancer une exception pour un cas évident (utilisateur non trouvé)
             }
 
             return $user;
         } catch (UserNotFoundException $exception) {
             $log->log($exception->getMessage());
+            // attraper l'exception juste après l'avoir lancée
             
             return null;
         } catch (Exception $exception) {
             $log->log($exception->getMessage());
+            //même que l'en haut 
 
             return null;
         }
@@ -44,8 +51,10 @@ class Controller {
     public function getCurrentUser(string $id) : ?string 
     {
         $log = new Logger();
+        // trop de redondance pour le logger
         try {
             $repository = new UserRepository();
+            //  instanciation du repository dans la méthode , pourquoi pas l'injecter
             $user = $repository->getUserById($id);
             if (!$user) {
                 throw new UserNotFoundException();
@@ -58,7 +67,8 @@ class Controller {
             return "User not found !";
         } catch (Exception $exception) {
             $log->log("Internal serveur error, {$exception->getMessage()}");
-
+            // attraper une exception sans traiter le cas
+            // error bilingue ? 
             return "Une erreur est survenue !";
         }
     }
